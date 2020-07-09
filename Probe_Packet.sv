76fe4c6c1e563b78242c715164c8cb52a092c9c4
selvaruban johnson <45601185+selvaruban-johnson@users.noreply.github.com>
2020-07-09 16:00:01 +0530

    Create Probe_Packet.sv

---
+++ Probe_Packet.sv
@@ -0,0 +1,16 @@
+class Probe_Packet;
+
+		logic psel,penable,pwrite;
+		logic [7 : 0] paddr,pwdata,prdata;
+		logic pready;
+
+
+	function void pre_randomize();
+		
+	endfunction
+
+	function void post_randomize();
+		
+	endfunction
+
+endclass
